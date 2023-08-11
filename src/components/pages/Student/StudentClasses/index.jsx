daf70681c3a8c51c9019dd0a09436e6d88aa3e3e
zeinaCodes <eye_likemoon@hotmail.com>
2023-08-12 00:39:14 +0300

    <student> added colors.css

--- src/components/pages/Student/StudentClasses/index.jsx
+++ src/components/pages/Student/StudentClasses/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Navbar from '../../../shared/navbar'
-import Card from '../../../shared/Card'
+import CategoriesContainer from '../../../Student/CategoriesContainer'
 import "./style.css"
+import CardsContainer from '../../../shared/CardsContainer'
 
 const StudentClasses = () => {
 
   return (
     <div>
       <Navbar one={'Classwork'} two={'Enrollments'}/>
+      <CategoriesContainer/>
+      <CardsContainer/>
     </div>
   )
 }
