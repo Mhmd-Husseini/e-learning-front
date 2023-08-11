491ba9213ba87335c81c4383d06e32a014019005
zeinaCodes <eye_likemoon@hotmail.com>
2023-08-11 14:52:14 +0300

    <student> added card to shared components

--- src/components/pages/Student/StudentClasses/index.jsx
+++ src/components/pages/Student/StudentClasses/index.jsx
@@ -1,21 +1,13 @@
 import React from 'react'
+import Navbar from '../../../shared/navbar'
+import Card from '../../../shared/Card'
 import "./style.css"
 
 const StudentClasses = () => {
+
   return (
     <div>
-      <div className="nav">
-        <div className="upper-nav">
-          <div className="nav-left">logo</div>
-          <div className="nav-middle"></div>
-          <div className="nav-right">
-            <div className="nav-links">Extra</div>
-            <div className="nav-links">Enrollments</div>
-            <div className="nav-links">Logout</div>
-          </div>
-        </div>
-        <div className='heroo'>Welcome Student</div>
-      </div>
+      <Navbar one={'Classwork'} two={'Enrollments'}/>
     </div>
   )
 }
---
+++ src/components/shared/Card/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div className="container">
+            <div className="content">
+                <a href="">
+                    <div className="card">
+                    </div></a>
+            </div>
+        </div>
+    )
+}
+
+export default Card
