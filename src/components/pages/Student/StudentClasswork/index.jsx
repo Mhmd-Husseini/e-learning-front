a740473ac15662be6a597793dcb5b602874cc2ff
zeinaCodes <eye_likemoon@hotmail.com>
2023-08-12 02:23:22 +0300

    <student> added StudentClasswork page

---
+++ src/components/pages/Student/StudentClasswork/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../../shared/navbar'
+
+
+const StudentClasswork = () => {
+  return (
+    <div>
+      <Navbar one={'Classwork'} two={'Enrollments'}/>
+    </div>
+  )
+}
+
+export default StudentClasswork
