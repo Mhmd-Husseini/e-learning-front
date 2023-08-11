02e3bec2df67b120cfea19e6a9a9636194b0066f
zeinaCodes <eye_likemoon@hotmail.com>
2023-08-11 14:12:50 +0300

    <student> implemented navbar

--- src/components/pages/Student/StudentClasses/index.jsx
+++ src/components/pages/Student/StudentClasses/index.jsx
@@ -3,7 +3,20 @@ import "./style.css"
 
 const StudentClasses = () => {
   return (
-    <div>index</div>
+    <div>
+      <div className="nav">
+        <div className="upper-nav">
+          <div className="nav-left">logo</div>
+          <div className="nav-middle"></div>
+          <div className="nav-right">
+            <div className="nav-links">Extra</div>
+            <div className="nav-links">Enrollments</div>
+            <div className="nav-links">Logout</div>
+          </div>
+        </div>
+        <div className='heroo'>Welcome Student</div>
+      </div>
+    </div>
   )
 }
 
