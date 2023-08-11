1a92cf123d14a127106aeb69d929623da1fec4f0
zeinaCodes <eye_likemoon@hotmail.com>
2023-08-11 14:52:14 +0300

    <client> transferred course card from student branch

--- src/components/pages/Student/StudentClasses/index.jsx
+++ src/components/pages/Student/StudentClasses/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Navbar from '../../../shared/navbar'
+import Card from '../../../shared/Card'
 import "./style.css"
 
 const StudentClasses = () => {
+
   return (
-    <div>index</div>
+    <div>
+      <Navbar one={'Classwork'} two={'Enrollments'}/>
+    </div>
   )
 }
 
