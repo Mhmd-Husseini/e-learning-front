b66f3e51b0fec456a84a6ef88b3ce55de8612464
zeinaCodes <eye_likemoon@hotmail.com>
2023-08-12 02:18:20 +0300

    <student> modified StudentCourse page

--- src/components/Student/CourseDetails/index.jsx
+++ src/components/Student/CourseDetails/index.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import './style.css'
 
-const CourseDetails = ({teacher, description}) => {
-  return (
-    <div className='course-details'>
-        <div className="course-wrapper">
-            <span className='title-large'>Taught By {teacher}</span>
-            <div><span className='title-medium'>Course Description:</span>
-            <p className=''>{description}</p>
+const CourseDetails = ({ teacher, description }) => {
+    return (
+        <div className='course-details'>
+            <div className="course-wrapper">
+                <span className='title-large'>Taught By {teacher}</span>
+                <div>
+                    <span className='title-medium'>Course Description:</span>
+                    <p className=''>{description}</p>
+                </div>
+                <button className='enroll-btn'>Enroll</button>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default CourseDetails
