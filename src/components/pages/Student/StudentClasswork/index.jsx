4912b3586ff1af05586abd1b2bdfd5e9f807c4fd
zeinaCodes <eye_likemoon@hotmail.com>
2023-08-12 02:37:10 +0300

    <student> added StudentClasswork page

--- src/components/pages/Student/StudentClasswork/index.jsx
+++ src/components/pages/Student/StudentClasswork/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../../../shared/navbar'
+import ClassworkListContainer from '../../../Student/ClassworkListContainer'
 
 
 const StudentClasswork = () => {
   return (
     <div>
       <Navbar one={'Classwork'} two={'Enrollments'}/>
+      <ClassworkListContainer />
     </div>
   )
 }
