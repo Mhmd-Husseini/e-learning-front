1e9cd7145690cb057a7c06dc4d57eb983910afbc
zeinaCodes <eye_likemoon@hotmail.com>
2023-08-12 02:13:30 +0300

    <student> implemented StudentCourse structure

---
+++ src/components/Student/CourseDetails/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.css'
+
+const CourseDetails = ({teacher, description}) => {
+  return (
+    <div className='course-details'>
+        <div className="course-wrapper">
+            <span className='title-large'>Taught By {teacher}</span>
+            <div><span className='title-medium'>Course Description:</span>
+            <p className=''>{description}</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CourseDetails
--- src/components/pages/Student/StudentCourse/index.jsx
+++ src/components/pages/Student/StudentCourse/index.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './style.css'
 import Navbar from '../../../shared/navbar'
 import Hero from '../../../Student/Hero'
+import CourseDetails from '../../../Student/CourseDetails'
+
 
 
 const StudentCourse = () => {
@@ -9,6 +11,7 @@ const StudentCourse = () => {
     <div>
       <Navbar one={'Classwork'} two={'Enrollments'}/>
       <Hero course_name={"Math"} />
+      <CourseDetails teacher={"Quibusdam, consequuntur"} description={"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, consequuntur."} />
     </div>
   )
 }
