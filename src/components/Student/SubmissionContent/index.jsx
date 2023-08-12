7cd7097acb18bfcd608eb5824ced2d1271fc420d
zeinaCodes <eye_likemoon@hotmail.com>
2023-08-12 09:33:51 +0300

    <student> implemented StudentSubmission page structure

--- src/components/Student/SubmissionContent/index.jsx
+++ src/components/Student/SubmissionContent/index.jsx
@@ -5,9 +5,16 @@ const SubmissionContent = () => {
     return (
         <div className='submission-container'>
             <div className="submission-wrapper">
-                <div className='test'>test</div>
-                <div className='test'>test</div>
-                <div className='test'>test</div>
+                <div className='submission-instructions'>
+                    <span className='title-large'>Assignment Title</span>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et itaque porro fugit cupiditate voluptate. Repellendus unde qui, placeat optio enim id ullam dolorem eos numquam quae neque expedita, nemo tenetur!
+                    </p>
+                </div>
+                <div className='submission-upload'>
+                    <span className='title-medium'>Submit Work</span>
+                    <input type="file" />
+                </div>
             </div>
             <div className="submission-wrapper">
                 <button className='attend-btn'>Attendance</button>
