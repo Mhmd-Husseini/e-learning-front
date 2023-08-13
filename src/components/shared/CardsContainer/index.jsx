2f9af9815001405a20f019c3a7344cbbae1fb028
zeinaCodes <eye_likemoon@hotmail.com>
2023-08-13 21:21:00 +0300

    <student> modified cards container

--- src/components/shared/CardsContainer/index.jsx
+++ src/components/shared/CardsContainer/index.jsx
@@ -1,42 +1,57 @@
 import React from 'react'
 import Card from '../Card'
+import ClassworkListItem from '../../Student/ClassworkListItem'
+import Person from '../../Student/Person'
 import './style.css'
 
-const CardsContainer = ({courses, handleOpenModal}) => {
+const CardsContainer = ({element, data, handleOpenModal}) => {
 
     let sample_data =
-        [
-            {
-                course_id: 1,
-                name:"FSW",
-                description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde nobis deserunt corrupti velit!",
-            },
-            {
-                course_id: 2,
-                name:"UIX",
-                description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde nobis deserunt corrupti velit!",
-            },
-            {
-                course_id: 3,
-                name:"FSD",
-                description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde nobis deserunt corrupti velit!",
-            },
-            {
-                course_id: 3,
-                name:"FSBatata",
-                description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde nobis deserunt corrupti velit!",
-            },
-            {
-                course_id: 4,
-                name:"FSTe3tir",
-                description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde nobis deserunt corrupti velit!",
-            },
-        ]
+    [
+      {
+        id: 2,
+        course_id: 1,
+        title: "FSW",
+        name: "FSW",
+        due: "23:59 8-16-2023",
+      },
+      {
+        id: 2,
+        course_id: 2,
+        title: "UIX",
+        name: "UIX",
+        due: "23:59 8-16-2023",
+      },
+      {
+        id: 2,
+        course_id: 3,
+        title: "FSD",
+        name: "FSD",
+        due: "23:59 8-16-2023",
+      },
+      {
+        id: 2,
+        course_id: 3,
+        title: "FSBatata",
+        name: "FSBatata",
+        due: "23:59 8-16-2023",
+      },
+      {
+        id: 2,
+        course_id: 4,
+        title: "FSTe3tir",
+        name: "FSTe3tir",
+        due: "23:59 8-16-2023",
+      },
+    ]
   return (
     <div className='course-container'>
         <div className="course-content">
         {sample_data.map((item) => {
-            return <Card course={item} handleOpenModal={handleOpenModal}/>
+            return element=="course"? <Card course={item} handleOpenModal={handleOpenModal}/>
+            : element=="person" ?  <Person item={item} handleOpenModal={handleOpenModal}/>
+            : element=="task" ? <ClassworkListItem item={item}/>
+            : <div></div>   
         })}
         </div>
     </div>
